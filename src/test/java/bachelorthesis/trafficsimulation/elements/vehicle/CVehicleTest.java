3f3e699ad40df9e461ed706fc2fa3fdf8aba537e
Sven Albert-Pedersen <svalpe@gmail.com>
2018-02-25 23:36:21 +0100

    fix build

--- src/test/java/bachelorthesis/trafficsimulation/elements/vehicle/CVehicleTest.java
+++ src/test/java/bachelorthesis/trafficsimulation/elements/vehicle/CVehicleTest.java
@@ -9,12 +9,18 @@ import org.junit.Test;
 
 import javax.annotation.Nonnull;
 
-import static org.junit.Assert.*;
+/**
+ * test vehicle class components
+ */
+public class CVehicleTest
+{
 
-public class CVehicleTest {
-
+    /**
+     * test clipping of lane
+     */
     @Test
-    public void staticliteral() {
+    public void staticliteral()
+    {
         final Number l_viewrange = 250;
         final Number l_cellsize = 7.5;
 
@@ -39,24 +45,24 @@ public class CVehicleTest {
         );
     }
 
-    private int uncliptest( @Nonnull final DoubleMatrix1D p_position_first,
+    private int uncliptest( @Nonnull final DoubleMatrix1D p_positionfirst,
                                           @Nonnull final DoubleMatrix1D p_position,
-                                          @Nonnull final DoubleMatrix1D p_position_world,
+                                          @Nonnull final DoubleMatrix1D p_positionworld,
                                           @Nonnull final Number p_viewrangesize )
     {
         // calculate the range over the end of the lane, position is less than the environment size, nothing to do
-        System.out.println( p_position_first.getQuick( 1 ) + p_viewrangesize.doubleValue() );
-        System.out.println( p_position_world.getQuick( 1 ) );
-        if ( ( p_position_first.getQuick( 1 ) + p_viewrangesize.doubleValue() ) <= p_position_world.getQuick( 1 ) )
+        System.out.println( p_positionfirst.getQuick( 1 ) + p_viewrangesize.doubleValue() );
+        System.out.println( p_positionworld.getQuick( 1 ) );
+        if ( ( p_positionfirst.getQuick( 1 ) + p_viewrangesize.doubleValue() ) <= p_positionworld.getQuick( 1 ) )
             return 1;
 
         // if car is in-front of, nothing to do and inside the environment size
-        if ( p_position.getQuick( 1 ) >= p_position_first.getQuick( 1 ) )
+        if ( p_position.getQuick( 1 ) >= p_positionfirst.getQuick( 1 ) )
             return 2;
 
         // car is behind, so project car position in front-of (we know that the car is clipped)
         final DoubleMatrix1D l_project = p_position.copy();
-        l_project.setQuick( 1, l_project.getQuick( 1 ) + p_position_world.getQuick( 1 ) );
+        l_project.setQuick( 1, l_project.getQuick( 1 ) + p_positionworld.getQuick( 1 ) );
         return 3;
     }
 
@@ -75,12 +81,12 @@ public class CVehicleTest {
                 .assign( DoubleFunctions.mult( p_cellsize.doubleValue() ) );
     }
 
-    private DoubleMatrix1D unclip( @Nonnull final DoubleMatrix1D p_position_first,
+    private DoubleMatrix1D unclip( @Nonnull final DoubleMatrix1D p_positionfirst,
                                    @Nonnull final DoubleMatrix1D p_position,
-                                   @Nonnull final DoubleMatrix1D p_position_world,
+                                   @Nonnull final DoubleMatrix1D p_positionworld,
                                    @Nonnull final Number p_viewrangesize )
     {
-        return this.unclipforward( p_position_first, p_position, p_position_world, p_viewrangesize );
+        return this.unclipforward( p_positionfirst, p_position, p_positionworld, p_viewrangesize );
     }
 
     /**
@@ -89,34 +95,34 @@ public class CVehicleTest {
      * @param p_position world position of the other object
      * @return unclipped position data
      */
-    private DoubleMatrix1D unclipforward( @Nonnull final DoubleMatrix1D p_position_first,
+    private DoubleMatrix1D unclipforward( @Nonnull final DoubleMatrix1D p_positionfirst,
                                           @Nonnull final DoubleMatrix1D p_position,
-                                          @Nonnull final DoubleMatrix1D p_position_world,
+                                          @Nonnull final DoubleMatrix1D p_positionworld,
                                           @Nonnull final Number p_viewrangesize )
     {
         // calculate the range over the end of the lane, position is less than the environment size, nothing to do
-        System.out.println( p_position_first.getQuick( 1 ) + p_viewrangesize.doubleValue() );
+        System.out.println( p_positionfirst.getQuick( 1 ) + p_viewrangesize.doubleValue() );
         System.out.println( p_position.getQuick( 1 ) );
-        System.out.println( p_position_world.getQuick( 1 ) );
+        System.out.println( p_positionworld.getQuick( 1 ) );
         System.out.println( "FIRST" );
-        if ( ( p_position_first.getQuick( 1 ) + p_viewrangesize.doubleValue() ) <= p_position_world.getQuick( 1 ) )
+        if ( ( p_positionfirst.getQuick( 1 ) + p_viewrangesize.doubleValue() ) <= p_positionworld.getQuick( 1 ) )
             return p_position;
 
         // if car is in-front of, nothing to do and inside the environment size
-        System.out.println( p_position_first.getQuick( 1 ) + p_viewrangesize.doubleValue() );
+        System.out.println( p_positionfirst.getQuick( 1 ) + p_viewrangesize.doubleValue() );
         System.out.println( p_position.getQuick( 1 ) );
-        System.out.println( p_position_world.getQuick( 1 ) );
+        System.out.println( p_positionworld.getQuick( 1 ) );
         System.out.println( "SECOND" );
-        if ( p_position.getQuick( 1 ) >= p_position_first.getQuick( 1 ) )
+        if ( p_position.getQuick( 1 ) >= p_positionfirst.getQuick( 1 ) )
             return p_position;
 
         // car is behind, so project car position in front-of (we know that the car is clipped)
-        System.out.println( p_position_first.getQuick( 1 ) + p_viewrangesize.doubleValue() );
+        System.out.println( p_positionfirst.getQuick( 1 ) + p_viewrangesize.doubleValue() );
         System.out.println( p_position.getQuick( 1 ) );
-        System.out.println( p_position_world.getQuick( 1 ) );
+        System.out.println( p_positionworld.getQuick( 1 ) );
         System.out.println( "THIRD" );
         final DoubleMatrix1D l_project = p_position.copy();
-        l_project.setQuick( 1, l_project.getQuick( 1 ) + p_position_world.getQuick( 1 ) );
+        l_project.setQuick( 1, l_project.getQuick( 1 ) + p_positionworld.getQuick( 1 ) );
         System.out.println( l_project.getQuick( 1 ) );
         return l_project;
     }
